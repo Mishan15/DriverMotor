66875812e6d4c04a0d4077a59490c4bc02335e8c
misha <mishasherbakov150@gmail.com>
2020-03-07 22:56:47 +0500

    добавлена поддержка мотор-шилда от ампери

--- driverMotor.cpp
+++ driverMotor.cpp
@@ -17,6 +17,22 @@ driverMotor::driverMotor(byte leftM1, byte leftM2, byte rightM1, byte rightM2){
     _driver_type = 0;
 }
 
+driverMotor::driverMotor(byte leftDr, byte leftSpd, byte rightDr, byte rightSpd, char type){
+    if(type == 'a'){
+        _leftSpd = leftSpd;
+        _rightSpd = rightSpd;
+        _leftDr = leftDr;
+        _rightDr = rightDr;
+        pinMode(_leftDr, OUTPUT);
+        pinMode(_leftSpd, OUTPUT);
+        pinMode(_rightDr, OUTPUT);
+        pinMode(_rightSpd, OUTPUT);
+        digitalWrite(_leftSpd, LOW);
+        digitalWrite(_rightSpd, LOW);
+        _driver_type = 2;
+    }
+}
+
 driverMotor::driverMotor(byte leftM1, byte leftM2, byte leftMS, byte rightM1, byte rightM2, byte rightMS, bool use_as_def = 1){
     _leftM1 = leftM1;
     _leftM2 = leftM2;
@@ -77,6 +93,22 @@ void driverMotor::drive(bool directionLeft, byte speedLeft, bool directionRight,
             digitalWrite(_rightM1, HIGH);
             digitalWrite(_rightM2, LOW);
             analogWrite(_rightMS, speedRight);
+        }
+    }else if(_driver_type == 2){
+        if(directionLeft){
+            digitalWrite(_leftDr, HIGH);
+            analogWrite(_leftSpd, speedLeft);
+        }else{
+            digitalWrite(_leftDr, LOW);
+            analogWrite(_leftSpd, speedLeft);
+        }
+
+        if(directionRight){
+            digitalWrite(_rightDr, HIGH);
+            analogWrite(_rightSpd, speedLeft);
+        }else{
+            digitalWrite(_rightDr, LOW);
+            analogWrite(_rightSpd, speedLeft);
         }
     }
 }
--- driverMotor.h
+++ driverMotor.h
@@ -4,11 +4,13 @@
 class driverMotor{
     public:
         driverMotor(byte leftM1, byte leftM2, byte rightM1, byte rightM2); // инициализация поумолчанию. 
+        driverMotor(byte leftDr, byte leftSpd, byte rightDr, byte rightSpd, char type);
         driverMotor(byte leftM1, byte leftM2, byte leftMS, byte rightM1, byte rightM2, byte rightMS, bool use_as_def = 1); // инициализвция для 6-пинового интерфейса. 
         void drive(bool directionLeft, byte speedLeft, bool directionRight, byte speedRight);
         void stop();
     private:
         byte _leftM1, _leftM2, _rightM1, _rightM2, _leftMS, _rightMS;
+        byte _leftDr, _leftSpd, _rightDr, _rightSpd;
         byte _driver_type = 0;
 };
 
